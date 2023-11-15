fe062a5a362590d14b499d16fbf7d350ee97815f
Moyboy <Vincent.moy94@gmail.com>
2023-11-14 18:47:48 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (saturday = "roller-skate") {
+    return (`This Saturday, I want to ${saturday}!`);
+}
+
+function mondayWork (monday = "go to the office") {
+    return (`This Monday, I will ${monday}.`);
+}
+
+function wrapAdjective (visualFlair = "*") {
+    return function (description = "spcial") {
+        return `You are ${visualFlair}${description}${visualFlair}!`;
+    }
+}
